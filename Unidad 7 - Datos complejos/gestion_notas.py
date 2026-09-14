20d8b6c1a3519e08e7a6d8e9b532f96fbb7fe12d
AyrtonCalderon <146039167+AyrtonCalderon@users.noreply.github.com>
2026-09-14 18:31:50 -0300

    Correccion ejercicio B

--- Unidad 7 - Datos complejos/gestion_notas.py
+++ Unidad 7 - Datos complejos/gestion_notas.py
@@ -36,34 +36,34 @@ notasFinales = []
 # Recorremos el diccionario de alumnos (Legajo y Nombre)
 for legajo,nombre in alumnos.items():
     print(f"{nombre}")
-    suma_promedio_materias = 0 # Para acumular las notas del alumno
+    suma_promedio_materia = 0 # Para acumular las notas del alumno
 
 #Por cada alumno, recorremos la lista de materias
-for mat in materias:
-    nombre_materia = mat[0]
-    print(f"Ingrese las notas para {nombre_materia}: ")
+    for mat in materias:
+        nombre_materias = mat[0]
+        print(f"Ingrese las notas para {nombre_materias}: ")
 
     # Pedir Nota 1 con validación (0 al 10)
-    nota_1 = float(input("Nota 1: "))
-    while nota_1 < 0 or nota_1 > 10:
-        print("Error, la nota ingresada debe ser del 0 al 10")
-        nota1 = float(input("Nota 1: "))
+        nota_1 = float(input("Nota 1: "))
+        while nota_1 < 0 or nota_1 > 10:
+            print("Error, la nota ingresada debe ser del 0 al 10")
+            nota_1 = float(input("Nota 1: "))
 
     # Pedir Nota 2 con validación (0 al 10)
-        nota2 = float(input("Nota 2: "))
-        while nota2 < 0 or nota2 > 10:
+        nota_2 = float(input("Nota 2: "))
+        while nota_2 < 0 or nota_2 > 10:
             print("Error, la nota ingresada debe ser del 0 al 10")
-            nota2 = float(input("Nota 2: "))
+            nota_2 = float(input("Nota 2: "))
 
         # Calcular el promedio de esta materia
-        promedio_materia = (nota1 + nota2) / 2
-        print(f"Nota Final en {nombre_materia}: {promedio_materia}")
+        promedio_materia = (nota_1 + nota_2) / 2
+        print(f"Nota Final en {nombre_materias}: {promedio_materia}")
 
         # Acumulamos para el promedio general del alumno
-        suma_promedios_materias += promedio_materia
+        suma_promedio_materia += promedio_materia
 
     # 4. Al terminar todas las materias del alumno, calculamos su promedio general
-    promedio_general = suma_promedios_materias / len(materias)
+    promedio_general = suma_promedio_materia / len(materias)
 
     # 5. Guardamos [Nombre, Promedio General] en la lista notasFinales
     notasFinales.append([nombre, promedio_general])
